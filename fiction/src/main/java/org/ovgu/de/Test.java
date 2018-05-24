ac4b04f1bc99e9b88af10b95aff3cc8b6f6e0e7e
Adarsh-g-s <adarsh431593@gmail.com>
2018-05-24 14:35:35 +0200

    Add files via upload

---
+++ fiction/src/main/java/org/ovgu/de/Test.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.ovgu.de.tune2.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Suhita Ghosh
+ *
+ */
+public class Test {
+	private static int counter;
+
+	public static void main(String[] args) {/*
+		List<> options = new ArrayList<>();
+		Answer a = new Answer(1, options);
+		List<Question> questions = new ArrayList<>();
+
+		questions.add(new Question(1, "Whats ur name?", new Answer(1, new String[] { "a", "b", "C", "D" })));
+		questions.add(new Question(2, "Whats ur name1?", new Answer(1, new String[] { "a", "b", "C", "D" })));
+		questions.add(new Question(3, "Whats ur name2?", new Answer(1, new String[] { "a", "b", "C", "D" })));
+		questions.add(new Question(4, "Whats ur name3?", new Answer(1, new String[] { "a", "b", "C", "D" })));
+		questions.add(new Question(5, "Whats ur name4?", new Answer(1, new String[] { "a", "b", "C", "D" })));
+
+		Random r = new Random();
+
+		counter = questions.size();
+		int n =r.nextInt(counter--);
+		System.out.println(questions.get(n).getText()+questions.size());
+		questions.remove(questions.get(n));
+		 n =r.nextInt(counter--);
+		System.out.println(questions.get(n).getText()+questions.size());
+		questions.remove(questions.get(n));
+		 n =r.nextInt(counter--);
+		System.out.println(questions.get(n).getText()+questions.size());
+		questions.remove(questions.get(n));
+		 n =r.nextInt(counter--);
+		System.out.println(questions.get(n).getText()+questions.size());
+		questions.remove(questions.get(n));
+		 //n =r.nextInt(length-5);
+		System.out.println(questions.get(0).getText()+questions.size());
+		
+
+	*/}
+}
