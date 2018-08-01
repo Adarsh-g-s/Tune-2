987f15a407d54794e740756619c8b6c769c4657f
Subash Prakash <subashprakash8@gmail.com>
2018-08-02 00:49:10 +0200

    Initial version of parser committed

---
+++ fiction/src/main/utility/parser/json_parser_config.py
@@ -0,0 +1,23 @@
+#Defining Some configurations required for the project
+#Author: Subash Prakash
+
+
+#Defines the input Directory
+inputDir="G:\Tune-2\json-parser\parser\input"
+
+#Defines the output Directory
+outputDir=""
+
+#Defines Processed Directory
+processDir=""
+
+#Zip Names
+zipFileLocation = "zipFileLocation"
+zipNames = "200_tweets,500_tweets"
+
+#Defines Log Directory
+logDir=""
+
+
+#Defines What type of file do you want (Give anything like csv, txt, prop as options) Please default to prop only!!
+option="prop"
